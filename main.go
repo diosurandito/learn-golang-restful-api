cea0f02f95aaa150b41a79b7e2952c2e76fa8476
diosurandito <diosurandito@gmail.com>
2021-12-30 10:59:41 +0700

    20.HTTP Server

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"golang-restful-api/app"
 	"golang-restful-api/controller"
+	"golang-restful-api/helper"
 	"golang-restful-api/repository"
 	"golang-restful-api/service"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/julienschmidt/httprouter"
@@ -24,4 +28,13 @@ func main() {
 	router.GET("/api/categories/:categoryId", categoryController.FindById)
 	router.PUT("/api/categories/:categoryId", categoryController.Update)
 	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+
 }
